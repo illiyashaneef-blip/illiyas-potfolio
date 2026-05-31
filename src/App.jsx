0118fecb318e02a21335d80728b77c9ffe3a4cc3
illiyashaneef-bilp <illiyashaneef@gmail.com>
2026-05-31 16:14:20 +0100

    JK NM

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 
 import "./App.css";
-import Projects from "./components/projects";
+import Projects from "./components/Projects";
 
 function App() {
   return (
